dd2895e0eff17a771808f156df9262d12fad45ca
harikrishna-dev-max <harikrishnasiliveru1@gmail.com>
2026-01-04 14:20:36 +0530

    Add files via upload

---
+++ headers/myheader.h
@@ -0,0 +1,18 @@
+#ifndef __MYHEADER_H__
+#define __MYHEADER_H__
+#include "types.h"
+#include "adc.h"
+#include "adcdefines.h"
+#include "defines.h"
+#include "delay.h"
+#include "eint0_irq.h"
+#include "keyFunction.h"
+#include "kpm.h"
+#include "kpmdefines.h"
+#include "lcd.h"
+#include "lcddefines.h"
+#include "pin_connct_block.h"
+#include "pin_function_defines.h"
+#include "rtc.h"
+#include "rtcdefines.h"
+#endif
